91a00ccd8edddfd83d510114503e4109eb8ee65f
Rowan Rodrik van der Molen <rowan@ytec.nl>
2021-10-19 17:27:01 +0100

    Cumbersome progress; I'm no procedural hero (yet) 🦸

--- xjot_to_xml.c
+++ xjot_to_xml.c
@@ -15,14 +15,29 @@ struct
 {
 } xjot_attr;
 
+/**
+ * Not every `xjot_node_type` corresponds to a precise XML node type.
+ * When encountering a '<', for example, the state is `XjotPointyOpening`,
+ * because the stream reader cannot determine yet at that point whether it
+ * will be an element, a processing instruction or a comment that is being
+ * started at that position in the input.
+ */
 typedef enum xjot_node_type
 {
     XjotRootNode,
-    XjotElement,
+    XjotPointyOpening,
+    XjotElementName,
+    XjotElementAttributes,
+    XjotElementAttributeName,
+    XjotElementAttributeValue,
+    XjotElementContent,
+    XjotElementInline,
+    XjotElementMultiline,
     XjotComment,
+    XjotCommentInline,
+    XjotCommentMultiline,
     XjotProcessingInstruction,
     XjotText,
-    XjotMaybeElementOrMaybeProcessingInstruction
 } xjot_node_type;
 
 typedef struct xjot_node
@@ -31,6 +46,8 @@ typedef struct xjot_node
     unsigned int node_depth;
     enum xjot_node_type node_type;
     char* node_name;
+    char* attr_name;
+    char attr_start_quote;
     unsigned int start_line;
     unsigned int start_column;
 } xjot_node;
@@ -42,7 +59,6 @@ xjot_node* _xjot_new_child_node(
         unsigned int start_column)
 {
     xjot_node* child_node = malloc(sizeof(xjot_node));
-    printf("Post-malloc\n");
     (*child_node).parent_node = parent;
     (*child_node).node_depth = (*parent).node_depth + 1;
     (*child_node).node_type = node_type;
@@ -64,25 +80,30 @@ xjot_node* _xjot_exit_child_node(xjot_node* child)
     return parent;
 }
 
+char* _xjot_append_char_to_string(char* str, char c)
+{
+    char* extended_string = (str == NULL ? calloc(2, sizeof(char)) : realloc(str, sizeof(char) * (strlen(str) + 1)));
+    return strncat(extended_string, &c, 1);
+}
+
 void _xjot_to_xml_with_buf(int in_fd, int out_fd, void* buf, size_t buf_size)
 {
-    int n;
-    int c;
     int line_no = 1;
     int col_no = 0;
-    char byte;
-
     xjot_node root = {
         .parent_node = NULL,
         .node_depth = 0,
         .node_type = XjotRootNode,
         .node_name = NULL,
+        .attr_name = NULL,
+        .attr_start_quote = '\0',
         .start_line = 1,
         .start_column = 1,
     };
     xjot_node* context = &root;
 
-    while ((n = read(in_fd, &buf, buf_size)) != 0)
+    int n;
+    while ((n = read(in_fd, buf, buf_size)) != 0)
     {
         if (n == -1)
         {
@@ -90,41 +111,113 @@ void _xjot_to_xml_with_buf(int in_fd, int out_fd, void* buf, size_t buf_size)
             {
                 continue;
             }
-            if (errno == EWOULDBLOCK || errno == EAGAIN)
+            if (errno == EAGAIN)
             {
             }
             fputs(strerror(errno), stderr);
             break;
         }
 
-        printf("a\n");
-        printf("b\n");
-
+        char previous_byte = '\0';
         // Loop through characters within buffer:
-        for (c = 0; c < n; c++)
+        for (int c = 0; c < n; c++)
         {
             col_no++;
 
-            // FIXME: Why do I get a segmentation fault as soon as I try to access a single byte in `buf`?
-            byte = ((char*)buf)[0];
+            char byte = ((char*)buf)[c];
 
             switch (byte)
             {
-                // TODO: \n\r support
                 case '\n':
-                    line_no++;
+                case '\r':
+                    if (byte != previous_byte || (previous_byte != '\n' && previous_byte != '\r'))
+                        line_no++;
                     col_no = 1;
+                    if ((*context).node_type != XjotElementContent)
+                    {
+                        putchar(byte);
+                    }
+                    if ((*context).node_type == XjotElementName)
+                    {
+                        (*context).node_type = XjotElementAttributes;
+                    }
                     break;
                 case '<':
-                    printf("Child\n");
-                    context = _xjot_new_child_node(
-                            context, XjotMaybeElementOrMaybeProcessingInstruction, line_no, col_no
-                    );
+                    context = _xjot_new_child_node(context, XjotPointyOpening, line_no, col_no);
                     break;
                 case '>':
+                    if ((*context).node_type == XjotElementContent)
+                    {
+                        printf("</%s>", (*context).node_name);
+                    }
+                    else if ((*context).node_type == XjotProcessingInstruction)
+                    {
+                        printf("?>");
+                    }
                     context = _xjot_exit_child_node(context);
                     break;
+                case '/':
+                    if ((*context).node_type == XjotElementName)
+                    {
+                        (*context).node_type == XjotElementContent;
+                        printf("<%s>", (*context).node_name);
+                    }
+                    break;
+                case '@':
+                    if ((*context).node_type == XjotElementAttributes)
+                    {
+                        (*context).node_type == XjotElementAttributeName;
+                    }
+                    break;
+                case '=':
+                    if ((*context).node_type == XjotElementAttributeName)
+                    {
+                        (*context).node_type == XjotElementAttributeValue;
+                        printf("%s=", (*context).attr_name);
+                    }
+                    break;
+                case '\'':
+                    if ((*context).node_type == XjotElementAttributeValue)
+                    {
+
+                    }
+                    break;
+                case '?':
+                    if ((*context).node_type == XjotPointyOpening)
+                    {
+                        (*context).node_type = XjotProcessingInstruction;
+                        printf("<?");
+                    }
+                    break;
+                default:
+                    if ((*context).node_type == XjotPointyOpening)
+                    {
+                        (*context).node_type = XjotElementName;
+                    }
+
+                    if ((*context).node_type == XjotElementName)
+                    {
+                        (*context).node_name = _xjot_append_char_to_string((*context).node_name, byte);
+                    }
+                    else if ((*context).node_type == XjotElementAttributeName)
+                    {
+                        (*context).attr_name = _xjot_append_char_to_string((*context).attr_name, byte);
+                    }
+                    else if ((*context).node_type == XjotElementContent)
+                    {
+                        putchar(byte);
+                    }
+                    else if ((*context).node_type == XjotProcessingInstruction)
+                    {
+                        putchar(byte);
+                    }
+                    else if ((*context).node_type == XjotProcessingInstruction)
+                    {
+                    }
+                    break;
             }
+
+            previous_byte = byte;
         }
     }
 }
